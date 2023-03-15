dd6e64873c72826208d74c0f8d349a59c3999dcd
Frances Caldwell <64825979+FrancesCaldwell@users.noreply.github.com>
2023-03-14 23:57:56 -0700

    Update slideshow.js

--- Spring Slideshow/slideshow.js
+++ Spring Slideshow/slideshow.js
@@ -1,6 +1,9 @@
+// on window onload, run the rotate function
 window.onload = rotate;
 
+// theImage index is set to the first image in the array
 var theImage = 0;
+// the array of imaegs
 var slideImages = new Array(
   "images/cherry-blossom.jpg",
   "images/lilac.jpg",
@@ -12,10 +15,13 @@ var slideImages = new Array(
 );
 
 function rotate() {
+  // the if statement iterates through the images in the array
   theImage++;
   if (theImage == slideImages.length) {
     theImage = 0;
   }
+  // this displays the image in the iteration
+  // each image is displayed after a specific amount of time in seconds
   document.getElementById("slideshow").src = slideImages[theImage];
   setTimeout(rotate, 3 * 1000);
 }
